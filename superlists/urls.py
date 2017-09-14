9f8fabb5068952f3ca1c7300444cbf0a6682aca4
Sergio Yanez <sergioy@amazon.com>
2017-09-14 11:31:53 -0700

    Heroku deployment

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from lists import views as list_views
 from lists import urls as list_urls
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/', include(list_urls))
+    url(r'^lists/', include(list_urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
